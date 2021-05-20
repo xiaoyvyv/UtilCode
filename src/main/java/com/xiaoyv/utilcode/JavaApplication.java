0b0edcad4c1dd46bdef851244c497dc937ee0da7
xiaoyvyv <wanghuaiyu@bailun.com>
2021-05-20 14:11:04 +0800

    Dev: readme

--- src/main/java/com/xiaoyv/utilcode/JavaApplication.java
+++ src/main/java/com/xiaoyv/utilcode/JavaApplication.java
@@ -9,12 +9,22 @@ import java.io.File;
  * @since 2021/5/20
  */
 public class JavaApplication {
+    private File cacheDir;
+    private File filesDir;
 
     public File getCacheDir() {
-        return new File("");
+        return cacheDir == null ? new File("D:/Temp") : cacheDir;
     }
 
     public File getFilesDir() {
-        return new File("");
+        return filesDir == null ? new File("D:/File") : filesDir;
+    }
+
+    public void setCacheDir(File cacheDir) {
+        this.cacheDir = cacheDir;
+    }
+
+    public void setFilesDir(File filesDir) {
+        this.filesDir = filesDir;
     }
 }
